cd71730876386465ec850a7629b2daa97ddd717a
Wayan Tisna <wayantisna74@gmail.com>
2022-06-15 08:43:31 +0800

    update ignore

--- .nuxt/middleware.js
+++
@@ -1,9 +0,0 @@
-const middleware = {}
-
-middleware['guest'] = require('..\\nuxt\\middleware\\guest.js')
-middleware['guest'] = middleware['guest'].default || middleware['guest']
-
-middleware['verification'] = require('..\\nuxt\\middleware\\verification.js')
-middleware['verification'] = middleware['verification'].default || middleware['verification']
-
-export default middleware
