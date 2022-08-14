86c625c096fd72965816b9c5d5de4887240cec4f
Wayan Tisna <wayantisna74@gmail.com>
2022-08-14 10:19:01 +0800

    update transaction

---
+++ database/migrations/2022_08_14_092223_add_created_by_to_transactions_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->unsignedBigInteger('created_by');
+        });
+
+        Schema::table('line_items', function (Blueprint $table) {
+            $table->unsignedBigInteger('created_by');
+        });
+
+        Schema::table('productions', function (Blueprint $table) {
+            $table->unsignedBigInteger('created_by');
+        });
+
+        Schema::table('production_items', function (Blueprint $table) {
+            $table->unsignedBigInteger('created_by');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            //
+        });
+    }
+};
